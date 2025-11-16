afb309acf9508b3c479798cbf36f4db766a5d4fb
ProjetosEltonLira <eltondelira@gmail.com>
2025-11-16 13:51:47 -0300

    Criação do projeto

---
+++ src/main/java/eltonlira/portifolio/gerador_numero_proposta/GeradorNumeroPropostaApplication.java
@@ -0,0 +1,13 @@
+package eltonlira.portifolio.gerador_numero_proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorNumeroPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorNumeroPropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eltonlira/portifolio/gerador_numero_proposta/GeradorNumeroPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package eltonlira.portifolio.gerador_numero_proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorNumeroPropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
